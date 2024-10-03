c0ab5d638f562485d16a69fdfea35e44a103f3c5
zakariabahlaoui <zakariabahlaoui93@gmail.com>
2024-10-03 01:55:17 +0100

    task 6

---
+++ 0x01-ES6_promise/6-final-user.js
@@ -0,0 +1,19 @@
+/* eslint-disable implicit-arrow-linebreak */
+import signUpUser from './4-user-promise';
+import uploadPhoto from './5-photo-reject';
+
+export default async function handleProfileSignup(
+  firstName,
+  lastName,
+  fileName,
+) {
+  const results = await Promise.allSettled([
+    signUpUser(firstName, lastName),
+    uploadPhoto(fileName),
+  ]);
+
+  return results.map((result) => ({
+    status: result.status,
+    value: result.status === 'fulfilled' ? result.value : `${result.reason}`,
+  }));
+}
